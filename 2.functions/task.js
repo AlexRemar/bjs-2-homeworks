7e229d6b1b1219ecc4e7bddf95f2660262820cce
Alex Remar <takoe.moscow@gmail.com>
2023-02-01 10:42:01 +0300

    Второе задание полностью

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,80 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = min + max;
+  
+  for (let i=0; i < arr.length; i<0){
+  if (i > max) {
+    max = arr[i];
+    sum += arr[i];
+  }
+  
+  else if (i < min) {
+    min = arr[i];
+    sum += arr[i];
+  }
+  }
+  let avg = sum/arr.length;
+  +avg.toFixed(2);
+  
+    return { min: min, max: max, avg: avg };
+  }
+  
+  
+  
+  
+  function summElementsWorker(...arr) {
+    arr.reduce(function(a,b){
+      return a + b;
+    });
+  }
+  
+  function differenceMaxMinWorker(...arr) {
+    let minNumber = math.min(arr);
+    let maxNumber = math.max(arr);
+    return maxNumber - minNumber;
+  
+  }
+  
+  function differenceEvenOddWorker(...arr) {
+    let sumEvenElement = 0;
+    let sumOddElement = 0;
+    for (let i=0; i < arr.length; i<0){
+      if (i % 2) {
+        sumEvenElement++
+      }
+      else {
+        sumOddElement++
+      }
+      return sumEvenElement - sumEvenElement;
+    }
+  }
+  
+  function averageEvenElementsWorker(...arr) {
+    let sumEvenElement = 0;
+    let countEvenElement = 0;
+    for (let i=0; i < arr.length; i<0){
+      if (i % 2) {
+        sumEvenElement += arr[i];
+        countEvenElement += 1;
+      }
+    }
+      return sumEvenElement/countEvenElement;
+  }
+  
 
-  return { min: min, max: max, avg: avg };
-}
 
-function summElementsWorker(...arr) {
 
-}
 
-function differenceMaxMinWorker(...arr) {
-
-}
-
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
+  function makeWork (arrOfArr, func) {
+    let maxWorkerResult = -Infinity;
+    for (let i=0; i < arrOfArr.length; i<0){
+      makeWork(...func) += arrOfArr[i];
+      const obj = makeWork(...func);
+      if (obj > maxWorkerResult) {
+        obj = maxWorkerResult;
+      }
+    }
+    return maxWorkerResult;
+  }
+  
