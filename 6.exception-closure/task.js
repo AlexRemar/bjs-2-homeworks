ca4ced0dbe157b5becf87ace2ac00ff2900c50e3
Alex Remar <takoe.moscow@gmail.com>
2023-02-20 14:45:22 +0300

    Решение

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,44 @@
-﻿
+﻿function parseCount(argument){
+    if((!Number.parseFloat(argument))){
+        throw new Error('Невалидное значение');
+    }
+    return Number.parseFloat(argument);
+}
+
+function validateCount(argument){
+    try{
+        parseCount(argument);
+    } catch(error) {
+            return error;    
+    }
+}
+
+class Triangle{
+    constructor(sideA,sideB,sideC){
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    
+
+    if((this.sideA + this.sideB < this.sideC) || 
+    (this.sideA + this.sideC < this.sideB) ||
+    (this.sideC + this.sideB < this.sideA)){
+        throw new Error('Треугольник с такими сторонами не существует');
+    }
+}
+
+
+
+
+get perimeter() {return (this.sideA + this.sideB + this.sideC)
+}
+get area() {let sqare = (Math.sqrt((this.perimeter/2)*((this.perimeter/2)
+-this.sideA)*((this.perimeter/2)-this.sideB)*((this.perimeter/2)
+-this.sideC)))
+return +sqare.toFixed(3);
+}
+
+
+    
+}
+
